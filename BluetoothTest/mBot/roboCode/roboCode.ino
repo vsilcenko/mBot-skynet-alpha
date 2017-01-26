2e186d3929ce42cb8c1d991a87902b042ec0dd6c
Valdemar Silcenko <vsilcenko@1clickfactory.com>
2017-01-26 14:08:40 +0200

    robot code

---
+++ BluetoothTest/mBot/roboCode/roboCode.ino
@@ -0,0 +1,119 @@
+#include <Arduino.h>
+#include <Wire.h>
+#include <SoftwareSerial.h>
+
+#include <MeAuriga.h>
+
+//Encoder Motor
+MeEncoderOnBoard Encoder_1(SLOT1);
+MeEncoderOnBoard Encoder_2(SLOT2);
+MeBluetooth bluetooth(PORT_3);
+
+void isr_process_encoder1(void)
+{
+      if(digitalRead(Encoder_1.getPortB()) == 0){
+            Encoder_1.pulsePosMinus();
+      }else{
+            Encoder_1.pulsePosPlus();
+      }
+}
+
+void isr_process_encoder2(void)
+{
+      if(digitalRead(Encoder_2.getPortB()) == 0){
+            Encoder_2.pulsePosMinus();
+      }else{
+            Encoder_2.pulsePosPlus();
+      }
+}
+
+void move(int direction, int speed)
+{
+      int leftSpeed = 0;
+      int rightSpeed = 0;
+      if(direction == 1){
+            leftSpeed = -speed;
+            rightSpeed = speed;
+      }else if(direction == 2){
+            leftSpeed = speed;
+            rightSpeed = -speed;
+      }else if(direction == 3){
+            leftSpeed = -speed;
+            rightSpeed = -speed;
+      }else if(direction == 4){
+            leftSpeed = speed;
+            rightSpeed = speed;
+      }
+      Encoder_1.setTarPWM(leftSpeed);
+      Encoder_2.setTarPWM(rightSpeed);
+}
+void moveDegrees(int direction,long degrees, int speed_temp)
+{
+      speed_temp = abs(speed_temp);
+      if(direction == 1)
+      {
+            Encoder_1.move(-degrees,(float)speed_temp);
+            Encoder_2.move(degrees,(float)speed_temp);
+      }
+      else if(direction == 2)
+      {
+            Encoder_1.move(degrees,(float)speed_temp);
+            Encoder_2.move(-degrees,(float)speed_temp);
+      }
+      else if(direction == 3)
+      {
+            Encoder_1.move(-degrees,(float)speed_temp);
+            Encoder_2.move(-degrees,(float)speed_temp);
+      }
+      else if(direction == 4)
+      {
+            Encoder_1.move(degrees,(float)speed_temp);
+            Encoder_2.move(degrees,(float)speed_temp);
+      }
+}
+
+double angle_rad = PI/180.0;
+double angle_deg = 180.0/PI;
+
+unsigned char table[128] = {0};
+
+void setup(){
+    //Set Pwm 8KHz
+    TCCR1A = _BV(WGM10);
+    TCCR1B = _BV(CS11) | _BV(WGM12);
+    TCCR2A = _BV(WGM21) | _BV(WGM20);
+    TCCR2B = _BV(CS21);
+     attachInterrupt(Encoder_1.getIntNum(), isr_process_encoder1, RISING);
+    attachInterrupt(Encoder_2.getIntNum(), isr_process_encoder2, RISING);
+    bluetooth.begin(115200);
+}
+
+void loop(){
+  int readdata = 0,i = 0,count = 0;
+  char outDat;
+  if (bluetooth.available())
+  {
+    while((readdata = bluetooth.read()) != (int)-1)
+    {
+      table[count] = readdata;
+      count++;
+      delay(1);
+    }
+    if(table[0] == 1){
+      move(4,100);
+      _delay(5);
+      move(1,0);
+      _loop();
+    }
+  }
+}
+
+void _delay(float seconds){
+    long endTime = millis() + seconds * 1000;
+    while(millis() < endTime)_loop();
+}
+
+void _loop(){
+    Encoder_1.loop();
+    Encoder_2.loop();
+}
